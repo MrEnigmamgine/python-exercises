0ae852d0322cfb661c1127dd6bb65bc6ce96bf44
MrEnigmamgine "Joshua Mayes <mayes.joshua.d@gmail.com>
2022-05-24 17:06:45 -0400

    introduction

---
+++ python_introduction_exercises.py
@@ -0,0 +1,18 @@
+# 4. Create a hello world program. Create a text file named python_introduction_exercises.py and write a program that prints "Hello, World!" to the console. 
+# Run this program from the command line.
+
+print("Hello, World!")
+
+# Inside of your hello world program, create a variable named greeting that contains the message that you will print to the console. 
+# Run your script interactively (with python -i python_introduction_exercises.py) and view the contents of the greeting variable.
+
+greeting = "Hello, World!"
+
+
+# crux@Enigma2:/mnt/a/Github/codeup-data-science/python-exercises$ python python_introduction_exercises.py -i
+# Hello, World!
+# crux@Enigma2:/mnt/a/Github/codeup-data-science/python-exercises$ python -i python_introduction_exercises.py
+# Hello, World!
+# >>> greeting
+# 'Hello, World!'
+# >>>
